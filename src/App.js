536f4f904cee8e28ed2606dbd9f021c5247101b2
Artyom <artyom.moskalew@thoughtworks.com>
2020-10-06 19:04:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [marginTop, setMarginTop] = useState(50);
+  const [counter, setCounter] = useState(0);
+  const [gewonnen, setGewonnen] = useState(false);
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+
+  useEffect(() => {
+    startNewGame();
+  }, []);
+
+  function startNewGame() {
+    setStartTime(Date.now());
+    setCounter(0);
+    setGewonnen(false);
+  }
+
+  function handleClick() {
+    let rand = Math.random();
+    setMarginTop(rand * 95);
+    setCounter(counter + 1);
+    if (counter > 0) {
+      gameFinished();
+    }
+  }
+
+  function gameFinished() {
+    setGewonnen(true);
+    setEndTime(Date.now());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          height: "10%",
+          width: "20%",
+          backgroundColor: "yellow",
+          marginTop: marginTop + "vh",
+          marginLeft: "50%",
+          display: gewonnen ? "none" : "block",
+          position: "fixed",
+          textAlign: "center"
+        }}
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        Hello World !
+      </div>
+      <div
+        style={{
+          height: "100px",
+          width: "200px",
+          backgroundColor: "green",
+          marginTop: 45 + "vh",
+          marginLeft: "50%",
+          display: gewonnen ? "block" : "none",
+          position: "absolute"
+        }}
+        onClick={() => startNewGame()}
+      >
+        Gewonnen!
+        <br />
+        Deine Zeit lautet: {(endTime - startTime) / 1000} sekunden
+      </div>
+    </>
   );
 }
 
